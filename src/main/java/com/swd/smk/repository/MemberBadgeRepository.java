ddd6a03222b9e7c30a89a0911e119fe3fa26f823
hoangtmse <hoangtmse183217@fpt.edu.vn>
2025-07-02 21:01:02 +0700

    updated v2.3

--- src/main/java/com/swd/smk/repository/MemberBadgeRepository.java
+++ src/main/java/com/swd/smk/repository/MemberBadgeRepository.java
@@ -3,6 +3,8 @@ package com.swd.smk.repository;
 import com.swd.smk.model.Badge;
 import com.swd.smk.model.jointable.MemberBadge;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,7 +12,8 @@ import java.util.List;
 @Repository
 public interface MemberBadgeRepository extends JpaRepository<MemberBadge, Long> {
 
-    List<Badge> findBadgesByMemberId(Long memberId);
+    @Query("SELECT mb.badge FROM MemberBadge mb WHERE mb.member.id = :memberId AND mb.status = 'ACTIVE'")
+    List<Badge> findBadgesByMemberId(@Param("memberId") Long memberId);
 
     boolean existsByMemberIdAndBadgeId(Long id, Long id1);
 }
